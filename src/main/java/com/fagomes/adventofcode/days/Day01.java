a19218d7df70cf0b6c67fd959f718ffb1aa7d48f
fagomes <falmeidagomes13@gmail.com>
2024-12-03 15:07:18 -0300

    Day01

--- src/main/java/com/fagomes/adventofcode/days/Day01.java
+++ src/main/java/com/fagomes/adventofcode/days/Day01.java
@@ -1,5 +1,13 @@
 package com.fagomes.adventofcode.days;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.fagomes.adventofcode.days.interfaces.AdventDay;
 import com.fagomes.adventofcode.parts.AdventInput;
 import com.fagomes.adventofcode.parts.enums.AdventParts;
@@ -9,10 +17,22 @@ public class Day01 implements AdventDay {
     @Override
     public String resolve(AdventInput adventInput, AdventParts adventPart) {
         String[] input = adventInput.getLines();
+        List<Long> leftSide = Arrays.asList(input).stream()
+            .map((line) -> Long.parseLong(line.substring(0, 1)))
+            .collect(Collectors.toList());
+        List<Long> rightSide = Arrays.asList(input).stream()
+            .map((line) -> Long.parseLong(line.substring(line.length() - 1)))
+            .collect(Collectors.toList());
 
-        System.out.println(input);
+        leftSide.sort((a, b) -> a.compareTo(b));
+        rightSide.sort((a, b) -> a.compareTo(b));
 
-        return null;
+        Long result = 0L;
+        for (int i = 0; i < input.length; i++) {
+            result += Math.abs(leftSide.get(i) - rightSide.get(i));
+        }
+
+        return String.valueOf(result);
     }
 
 }
